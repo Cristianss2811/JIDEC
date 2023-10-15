72fae05303dfcea48edf9954fe4fdaa49430f226
Cristianss2811 <cristianss.2811@gmail.com>
2023-10-15 12:24:13 -0600

    Algortimo y tabla completados

--- src/jide/Sintactico.java
+++ src/jide/Sintactico.java
@@ -9,7 +9,7 @@ public class Sintactico {
     public String error;
 
     private String simbolos[] = {"id", "num", "int", "float", "char", ",", ";", "+", "-", "*", "/", "=", "(", ")", "$", "P", "Tipo", "V", "A", "Exp", "E", "Term", "T", "F"};
-    private String producciones[] = {"P'>P", "P>Tipo id V", "P>A", "Tipo>int", "Tipo>float", "Tipo>char", "V>id V", "V>; P", "A>id = Exp ;", "Exp>+ Term E", "Exp>- Term E", "Exp>Term E", "E>+ Term E", "E>- Term E", "E>Vacia", "Term>F T", "T>* F T", "T>/ F T", "T>Vacia", "F>id", "F>num", "F>(Exp)"};
+    private String producciones[] = {"P'>P", "P>Tipo id V", "P>A", "Tipo>int", "Tipo>float", "Tipo>char", "V>, id V", "V>; P", "A>id = Exp ;", "Exp>+ Term E", "Exp>- Term E", "Exp>Term E", "E>+ Term E", "E>- Term E", "E>Vacia", "Term>F T", "T>* F T", "T>/ F T", "T>Vacia", "F>id", "F>num", "F>( Exp )"};
 
     private String tabla[][] = {
         {"I7", "error", "I4", "I5", "I6", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I1", "I2", "error", "I3", "error", "error", "error", "error", "error"},
@@ -27,7 +27,7 @@ public class Sintactico {
         {"I7", "error", "I4", "I5", "I6", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I22", "I2", "error", "I3", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "I23", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"I18", "I19", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I20", "error", "error", "error", "error", "error", "error", "error", "error", "I24", "error", "I17"},
-        {"I18", "I19", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I20", "error", "error", "error", "error", "error", "error", "error", "error", "I25", "error", "I27"},
+        {"I18", "I19", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I20", "error", "error", "error", "error", "error", "error", "error", "error", "I25", "error", "I17"},
         {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I26", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P18", "P18", "P18", "I30", "I31", "error", "error", "P18", "error", "error", "error", "error", "error", "error", "error", "error", "I29", "error"},
         {"error", "error", "error", "error", "error", "error", "P19", "P19", "P19", "P19", "P19", "error", "error", "P19", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
@@ -36,7 +36,7 @@ public class Sintactico {
         {"error", "error", "error", "error", "error", "I11", "I12", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I33", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "P7", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "P8", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
-        {"error", "error", "error", "error", "error", "error", "P14", "I27", "I26", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I34", "error", "error", "error"},
+        {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I34", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I35", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P11", "error", "error", "error", "error", "error", "error", "P11", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"I18", "I19", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I20", "error", "error", "error", "error", "error", "error", "error", "error", "I36", "error", "I17"},
@@ -47,11 +47,11 @@ public class Sintactico {
         {"error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "I40", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "P6", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P9", "error", "error", "error", "error", "error", "error", "P9", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
-        {"error", "error", "error", "error", "error", "error", "P10", "error", "error", "error", "error", "error", "error", "P10", "error", "error", "error", "error", "error", "error", "I41", "error", "error", "error"},
+        {"error", "error", "error", "error", "error", "error", "P10", "error", "error", "error", "error", "error", "error", "P10", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
+        {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I41", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "I42", "error", "error", "error"},
-        {"error", "error", "error", "error", "error", "error", "P14", "I27", "I28", "error", "error", "error", "error", "P14", "error", "error", "error", "error", "error", "error", "error", "error", "I43", "error"},
+        {"error", "error", "error", "error", "error", "error", "P18", "P18", "P18", "I30", "I31", "error", "error", "P18", "error", "error", "error", "error", "error", "error", "error", "error", "I43", "error"},
         {"error", "error", "error", "error", "error", "error", "P18", "P18", "P18", "I30", "I31", "error", "error", "P18", "error", "error", "error", "error", "error", "error", "error", "error", "I44", "error"},
-        {"error", "error", "error", "error", "error", "error", "P18", "P18", "P18", "I30", "I31", "error", "error", "P18", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P21", "P21", "P21", "P21", "P21", "error", "error", "P21", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P12", "error", "error", "error", "error", "error", "error", "P12", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
         {"error", "error", "error", "error", "error", "error", "P13", "error", "error", "error", "error", "error", "error", "P13", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"},
